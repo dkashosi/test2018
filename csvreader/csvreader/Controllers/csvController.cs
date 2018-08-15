8a6325e05a22fd2522b114c28f8def5f281d4362
daniel kashosi <dkashosi@uco.edu>
2018-08-15 13:17:03 -0500

    x

---
+++ csvreader/csvreader/Controllers/csvController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using csvreader.Models;
+using System.IO;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace csvreader.Controllers
+{
+    public class csvController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Welcome(string name, int numTimes)
+        {
+
+            List<csv> myList = readData();
+
+
+            return View(myList);
+        }
+
+        private List<csv> readData()
+        {
+            List<csv> myList = new List<csv>();
+            string path = @"C:\Users\danie\source\repos\csvreader\data.csv";
+            if (!System.IO.File.Exists(path))
+            {
+                csv temp = new csv();
+                temp.AccountNumber = "Null";
+                myList.Add(temp);
+                return myList;
+                
+            }
+            else
+            {
+                using (StreamReader sr = System.IO.File.OpenText(path))
+                {
+                    string s = "";
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(s);
+                        csv temp = parseLine(s);
+                        myList.Add(temp);
+                        
+                    }
+                }
+                return myList;
+            }
+        }
+        private csv parseLine(string s)
+        {
+            csv Account = new csv();
+            int nextIndex = 0;
+            nextIndex = s.IndexOf(',');
+
+            Account.AccountNumber = s.Substring(0, nextIndex );
+            s = s.Substring(nextIndex+1);
+            nextIndex = s.IndexOf(',');
+            Account.LastName = s.Substring(0, nextIndex);
+            s = s.Substring(nextIndex + 1);
+            nextIndex = s.IndexOf(',');
+            Account.FirstName = s.Substring(0, nextIndex);
+            s = s.Substring(nextIndex + 1);
+            nextIndex = s.IndexOf(',');
+            Account.MiddleName = s.Substring(0, nextIndex);
+            s = s.Substring(nextIndex + 1);
+            nextIndex = s.IndexOf(',');
+
+            //get the adress city and state
+            Account.Address = s.Substring(0, nextIndex);
+            s = s.Substring(nextIndex + 1);
+            nextIndex = s.IndexOf(',');
+            Account.Address += s.Substring(0, nextIndex);
+            s = s.Substring(nextIndex + 1);
+            nextIndex = s.IndexOf(',');
+            Account.Address += s.Substring(0, nextIndex);
+            s = s.Substring(nextIndex + 1);
+            nextIndex = s.IndexOf(',');
+            Account.Address += s.Substring(0, nextIndex);
+            s = s.Substring(nextIndex + 1);
+            nextIndex = s.IndexOf(',');
+            Account.Address += s.Substring(0, nextIndex);
+            s = s.Substring(nextIndex + 1);
+            nextIndex = s.IndexOf(',');
+
+
+            Account.PhoneNumber = s.Substring(0, nextIndex);
+            s = s.Substring(nextIndex + 1);
+            nextIndex = s.IndexOf(',');
+            Account.SPhoneNumber = s.Substring(0, nextIndex);
+            s = s.Substring(nextIndex + 1);
+            nextIndex = s.IndexOf(',');
+            Account.OpendedDate = s.Substring(0, nextIndex);
+            s = s.Substring(nextIndex + 1);
+            nextIndex = s.IndexOf(',');
+            Account.Balance = s.Substring(0, nextIndex);
+            s = s.Substring(nextIndex + 1);
+            Account.Type = s;
+
+            return Account;
+        }
+
+    }
+}
---
+++ csvreader/csvreader/Models/csv.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace csvreader.Models
+{
+    public class csv
+    {
+        public String AccountNumber { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string SPhoneNumber { get; set; }
+        public string OpendedDate { get; set; }
+        public string Balance { get; set; }
+        public string Type { get; set; }
+
+    }
+}
